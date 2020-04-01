25346bb7340ce2e8c094c81b4537ebb9bb32278d
NikolaCGeorgiev <n.c.georgiev@gmail.com>
2020-04-01 09:56:00 +0300

    Seed admin user

--- Data/MyRockConcerts.Data/Seeding/RolesSeeder.cs
+++ Data/MyRockConcerts.Data/Seeding/RolesSeeder.cs
@@ -4,22 +4,23 @@
     using System.Linq;
     using System.Threading.Tasks;
 
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
     using MyRockConcerts.Common;
     using MyRockConcerts.Data.Models;
-
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.Extensions.DependencyInjection;
 
     internal class RolesSeeder : ISeeder
     {
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            await SeedRoleAsync(roleManager, GlobalConstants.AdministratorRoleName);
+            await SeedRoleAsync(roleManager, userManager, GlobalConstants.AdministratorRoleName);
         }
 
-        private static async Task SeedRoleAsync(RoleManager<ApplicationRole> roleManager, string roleName)
+        private static async Task SeedRoleAsync(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager, string roleName)
         {
             var role = await roleManager.FindByNameAsync(roleName);
             if (role == null)
@@ -28,6 +29,25 @@
                 if (!result.Succeeded)
                 {
                     throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+            }
+
+            if (!await userManager.Users.AnyAsync())
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    EmailConfirmed = true,
+                };
+
+                var password = "123456";
+
+                var result = await userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
                 }
             }
         }
