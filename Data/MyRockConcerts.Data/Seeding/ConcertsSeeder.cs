ee8ba40e4b22ee1885b374bb2e615f997519e19b
NikolaCGeorgiev <n.c.georgiev@gmail.com>
2020-12-03 14:53:41 +0200

    ConcertsSeeder change dates.

--- Data/MyRockConcerts.Data/Seeding/ConcertsSeeder.cs
+++ Data/MyRockConcerts.Data/Seeding/ConcertsSeeder.cs
@@ -18,18 +18,18 @@
 
             await dbContext.Concerts.AddAsync(new Concert
             {
-                Name = "Hills Of Rock 2020",
+                Name = "Hills Of Rock 2021",
                 ImgUrl = "https://res.cloudinary.com/nikolacgeorgiev/image/upload/v1587376364/concerts_photos/Hills_Of_Rock_2020_rcutks.jpg",
-                Date = DateTime.ParseExact("2020-10-24 18:00", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                Date = DateTime.ParseExact("2021-10-24 18:00", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
                 TicketUrl = "https://www.eventim.bg/bg/bileti/hills-of-rock-2020-plovdiv-rowing-canal-1181470/performance.html",
                 VenueId = 2,
             });
 
             await dbContext.Concerts.AddAsync(new Concert
             {
-                Name = "Varna Rock 2020",
+                Name = "Varna Rock 2021",
                 ImgUrl = "https://res.cloudinary.com/nikolacgeorgiev/image/upload/v1587376430/concerts_photos/Varna_Rock_2020_gwgfri.jpg",
-                Date = DateTime.ParseExact("2020-11-14 18:00", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                Date = DateTime.ParseExact("2021-11-14 18:00", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
                 TicketUrl = "https://www.eventim.bg/bg/bileti/varna-rock-2020-barna-plaz-asparuhovo-1148430/performance.html",
                 VenueId = 3,
             });
